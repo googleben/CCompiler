58ec43d42a0b8e18deb0afed089f9c9055426c7d
Ben <william.ben.wyatt@gmail.com>
2019-09-13 15:50:11 -0500

    Add more tokens

--- CCompiler/Program.fs
+++ CCompiler/Program.fs
@@ -1,31 +1,117 @@
 ﻿namespace CCompiler
+open System.Linq.Expressions
+
 module Lexer = 
     open System
     open System
     open System.IO
     open System.Text.RegularExpressions
 
+    exception UnexpectedEOF
+    exception UnexpectedCharacter of char
 
     type TokenType =
+        //special characters
         | OPEN_CURLY
         | CLOSE_CURLY
         | OPEN_PAREN
         | CLOSE_PAREN
+        | OPEN_SQUARE
+        | CLOSE_SQUARE
         | SEMICOLON
+        | PERIOD
+        | COMMA
+        | DOUBLE_QUOTE
+        | SINGLE_QUOTE
+        | PIPE
+        | DOUBLE_PIPE
+        | AMPERSAND
+        | DOUBLE_AMPERSAND
+        | CARET
+        | STAR
+        | PLUS
+        | MINUS
+        | EQUALS
+        | DOUBLE_EQUALS
+        | EXCLAMATION
+        //keywords
+        | AUTO
+        | BREAK
+        | CASE
+        | CHAR
+        | CONST
+        | CONTINUE
+        | DEFAULT
+        | DO
+        | DOUBLE
+        | ELSE
+        | ENUM
+        | EXTERN
+        | FLOAT
+        | FOR
+        | GOTO
+        | IF
         | INT
-        | VOID
+        | LONG
+        | REGISTER
         | RETURN
+        | SHORT
+        | SIGNED
+        | SIZEOF
+        | STATIC
+        | STRUCT
+        | SWITCH
+        | TYPEDEF
+        | UNION
+        | UNSIGNED
+        | VOID
+        | VOLATILE
+        | WHILE
         | INT_LITERAL
         | IDENT
         
     let getKeywordTokenType str =
         match str with
+            | "auto" -> Some AUTO
+            | "break" -> Some BREAK
+            | "case" -> Some CASE
+            | "char" -> Some CHAR
+            | "const" -> Some CONST
+            | "continue" -> Some CONTINUE
+            | "default" -> Some DEFAULT
+            | "do" -> Some DO
+            | "double" -> Some DOUBLE
+            | "else" -> Some ELSE
+            | "enum" -> Some ENUM
+            | "extern" -> Some EXTERN
+            | "float" -> Some FLOAT
+            | "for" -> Some FOR
+            | "goto" -> Some GOTO
+            | "if" -> Some IF
             | "int" -> Some INT
-            | "void" -> Some VOID
+            | "long" -> Some LONG
+            | "register" -> Some REGISTER
             | "return" -> Some RETURN
+            | "short" -> Some SHORT
+            | "signed" -> Some SIGNED
+            | "sizeof" -> Some SIZEOF
+            | "static" -> Some STATIC
+            | "struct" -> Some STRUCT
+            | "switch" -> Some SWITCH
+            | "typedef" -> Some TYPEDEF
+            | "union" -> Some UNION
+            | "unsigned" -> Some UNSIGNED
+            | "void" -> Some VOID
+            | "volatile" -> Some VOLATILE
+            | "while" -> Some WHILE
             | _ -> None
         
     type Token = {Pos:Tuple<int,int>; Type: TokenType; Src:string}
+    
+    let rec printTokens (tokens: Token list) =
+        match tokens with
+            | token::rest -> printf "%O%s" token.Type (if not (rest = []) && (fst (List.head rest).Pos) > (fst token.Pos) then "\n" else " "); printTokens rest
+            | [] -> ()
 
     let (|Newline|Whitespace|Semicolon|Other|) ch =
         if ch = '\n' then Newline
@@ -39,7 +125,24 @@ module Lexer =
             | "}" -> CLOSE_CURLY
             | "(" -> OPEN_PAREN
             | ")" -> CLOSE_PAREN
+            | "[" -> OPEN_SQUARE
+            | "]" -> CLOSE_SQUARE
             | ";" -> SEMICOLON
+            | "." -> PERIOD
+            | "," -> COMMA
+            | "\"" -> DOUBLE_QUOTE
+            | "'" -> SINGLE_QUOTE
+            | "|" -> PIPE
+            | "||" -> DOUBLE_PIPE
+            | "&" -> AMPERSAND
+            | "&&" -> DOUBLE_AMPERSAND
+            | "^" -> CARET
+            | "*" -> STAR
+            | "+" -> PLUS
+            | "-" -> MINUS
+            | "=" -> EQUALS
+            | "==" -> DOUBLE_EQUALS
+            | "!" -> EXCLAMATION
             | "int" -> INT
             | "void" -> VOID
             | "return" -> RETURN
@@ -55,6 +158,7 @@ module Lexer =
         while Char.IsLetter (file.Chars pos) do
             str <- str + (string (file.Chars pos))
             pos <- pos + 1
+            if pos = file.Length then raise UnexpectedEOF else ()
         match (getKeywordTokenType str) with
             | Some tt -> {Pos=(line, linePos'); Type=tt; Src=str}
             | None -> {Pos=(line, linePos'); Type=IDENT; Src=str}
@@ -65,6 +169,7 @@ module Lexer =
         while Char.IsNumber (file.Chars pos) do
             str <- str + (string (file.Chars pos))
             pos <- pos + 1
+            if pos = file.Length then raise UnexpectedEOF else ()
         {Pos=(line, linePos'); Type=INT_LITERAL; Src=str}
         
 
@@ -83,8 +188,21 @@ module Lexer =
                         | '(' -> [getToken "(" line linePos]
                         | ')' -> [getToken ")" line linePos]
                         | ';' -> [getToken ";" line linePos]
+                        | '.' -> [getToken "." line linePos]
+                        | ',' -> [getToken "," line linePos]
+                        | '\"' -> [getToken "\"" line linePos]
+                        | '\'' -> [getToken "'" line linePos]
+                        | '|' -> if (file.Chars pos') = '|' then pos' <- pos' + 1; linePos' <- linePos' + 1; [getToken "||" line linePos] else [getToken "|" line linePos]
+                        | '&' -> if (file.Chars pos') = '&' then pos' <- pos' + 1; linePos' <- linePos' + 1; [getToken "&&" line linePos] else [getToken "&" line linePos]
+                        | '^' -> [getToken "^" line linePos]
+                        | '*' -> [getToken "*" line linePos]
+                        | '+' -> [getToken "+" line linePos]
+                        | '-' -> [getToken "-" line linePos]
+                        | '=' -> if (file.Chars pos') = '=' then pos' <- pos' + 1; linePos' <- linePos' + 1; [getToken "==" line linePos] else [getToken "=" line linePos]
+                        | '!' -> [getToken "!" line linePos]
                         | c when Char.IsLetter(c) -> [getIdentOrKeyword c file &pos' &linePos' line]
                         | c when Char.IsNumber(c) -> [getNumber c file &pos' &linePos' line]
+                        | c -> raise (UnexpectedCharacter c)
             lex file pos' line' linePos' now
                
 
@@ -92,7 +210,7 @@ module Lexer =
         let file = file'.Replace("\r", "")
         printfn "%O" file
         let lexed = lex file 0 0 0 []
-        printfn "%A" lexed
+        printTokens lexed
 
     let fileToString (file:FileStream) =
         use reader = new StreamReader(file)
--- CCompiler/in.c
+++ CCompiler/in.c
@@ -1,3 +1,3 @@
 ﻿int main(void) {
-    return 2;
+    return 2 || 3 | 5 == 1;
 }
